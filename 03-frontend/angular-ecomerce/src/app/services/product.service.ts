cb55b5c8532465dea8e5300025f999dde91e8c4f
Plamen Dragiev <pl.dragiev@gmail.com>
2021-06-07 17:29:12 +0300

    adding fuctionality to search products by category

--- 03-frontend/angular-ecomerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecomerce/src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators'
+
 @Injectable({
   providedIn: 'root'
 })
@@ -11,8 +12,12 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { 
 }
-  getProductList(): Observable<Product[]>{
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+  getProductList(theCategoryId: number): Observable<Product[]>{
+
+    //  need to build URL based on category id ...
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
